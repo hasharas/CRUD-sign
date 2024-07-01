8f6239f7562e0ea4e0272042cfa28b729745c014
hasharas <hasharamsankalpam@gmail.com>
2024-07-01 16:22:05 +0530

    add usestate for createjs file

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,22 +1,36 @@
 import React from 'react'
+import  { useState } from 'react';
 import { Button, Form } from 'semantic-ui-react'
 
-const Create = () => (
+const Create = ()=> {
+      const [firstName, setFirstName] = useState('');
+      const [lastName, setLastName] = useState('');
+      const [Email, setEmail] = useState(false);
+      const postData = () => {
+          console.log(firstName);
+          console.log(lastName);
+          console.log(Email);
+      }
+
+return (
+      <div>
       <Form className="create-form">
         <Form.Field>
             <label>First Name</label>
-            <input placeholder='First Name' />
+            <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)} />
         </Form.Field>
         <Form.Field>
             <label>Last Name</label>
-            <input placeholder='Last Name' />
+            <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value) }/>
         </Form.Field>
         <Form.Field>
             <label>Email</label>
-            <input type='email' placeholder='Email' />
+            <input type='email' placeholder='Email' onChange={(e) => setEmail(e.target.value) } />
         </Form.Field>
-        <Button type='submit'>Submit</Button>
+        <Button onClick={postData} type='submit'>Submit</Button>
     </Form>
-)
+    </div>
+ )
+}
 
 export default Create;
